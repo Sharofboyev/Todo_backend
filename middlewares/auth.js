bfea9ad60aec2bb911b232cb3888939d4056a6ef
Sarvar <sharofboyev.sarvar@gmail.com>
2022-11-22 23:21:41 +0500

    Bug fix. Authorization middleware fixed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,11 +3,12 @@ const config = require("../config");
 
 async function auth(req, res, next) {
   try {
-    let decoded = jwt.verify(req.header.accessToken, config.secretKey);
+    let decoded = jwt.verify(req.headers.accesstoken, config.secretKey);
     req.userId = decoded.userId;
     return next();
   } catch (err) {
-    return res.status(401).send({ success: false, error: error.message });
+    console.log(err);
+    return res.status(401).send({ success: false, error: "You are not authorized" });
   }
 }
 
