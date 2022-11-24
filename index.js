7163638d207a96fb4236e7363da57ea48ca4bba5
Sharofboyev Sarvar PIS <sharofboyev.sarvar@gmail.com>
2022-11-24 08:35:15 +0500

    Set up cors middleware

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const config = require("./config");
 const router = require("./routers");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send({success: false, error: "Internal server error"});
-})
+  res.status(500).send({ success: false, error: "Internal server error" });
+});
 
 app.listen(config.appPort, () => {
   console.log(`Listening on port ${config.appPort}...`);
